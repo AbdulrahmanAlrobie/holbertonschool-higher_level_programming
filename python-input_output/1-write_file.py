c680d60165253cbcef7165a76800668cc7b2342a
abdulrahman <abdulrahman@gmail.com>
2025-06-19 12:53:38 +0000

    first task

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""Module containing a function to read a text file and print its content."""
+"""Module containing a function to write a string to a UTF-8 text file and return the number of characters written."""
 
 
-def read_file(filename=""):
-    """Reads a UTF-8 text file and prints its content to stdout."""
-    with open(filename, encoding="utf-8") as f:
-        print(f.read(), end="")
+def write_file(filename="", text=""):
+    """Writes a string to a UTF-8 text file and returns the number of characters written."""
+    with open(filename, 'w', encoding='utf-8') as f:
+        count = f.write(text)
+    return count
